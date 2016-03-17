bd50b07b1e36e9715222126c21fed75785fd738b
hzyiting <hzyiting@corp.netease.com>
2016-03-17 15:38:26 +0800

    add read configuration function.

--- synservice/service.py
+++ synservice/service.py
@@ -1,16 +1,27 @@
 from docker_client import DockerClient
 from git_cmd import GitClient
+import configparser
 
 __author__ = 'hzyiting'
 
 
 def main():
+    # read config file
+    cp=configparser.RawConfigParser()
+    cp.read("config.conf")
+    docker_repo=cp.get("docker","docker_repo")
+    docker_username=cp.get("docker","docker_username")
+    docker_password=cp.get("docker","docker_password")
+    docker_email=cp.get("docker","docker_email")
+    docker_nickname=cp.get("docker","docker_nickname")
+
+
     gitClient = GitClient()
     gitClient.pullRepo("https://github.com/yiting1122/official-images.git", "official-images")
     dockerRepoVersionMaps = gitClient.getDockerRepoVersion("official-images/library")
 
-    nickname = "nce2"
-    url = "localhost:5000"
+    # nickname = "nce_dev"
+    # url = "localhost:5000"
 
     dockerClient = DockerClient()
 
@@ -18,14 +29,13 @@ def main():
         versions = dockerRepoVersionMaps[key]
         for version in versions:
             if dockerClient.pullDockerRepo(key, version):
-                dockerClient.tagDockerRepo(key, version, nickname, url)
+                dockerClient.tagDockerRepo(key, version,  docker_repo,docker_nickname)
             else:
                 print("docker pull repo failed: " + key + ":" + version)
 
-    imageList = dockerClient.getDockerImages(url)
+    imageList = dockerClient.getDockerImages(docker_repo)
     if len(imageList) > 0:
-        """qaqance_dev@163.com/qa1234  qaqance_dev@163.com"""
-        if dockerClient.loginDocker("qaqance_dev@163.com", "qa1234", "qaqance_dev@163.com", url):
+        if dockerClient.loginDocker(docker_username, docker_password, docker_email, docker_repo):
             for image in imageList:
                 dockerClient.pushDockerRepo(image)
         else:
