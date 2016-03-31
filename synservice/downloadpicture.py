63ddaf404d32ae5e2f6a5fb3d4be42cbdae051f9
hzyiting <hzyiting@corp.netease.com>
2016-03-31 17:12:02 +0800

    add downloadall picture

--- synservice/downloadpicture.py
+++ synservice/downloadpicture.py
@@ -1,8 +1,14 @@
+from git_cmd import GitClient
+
 __author__ = 'hzyiting'
 import os
-
+from configobj import ConfigObj
 
 def main():
+    downloadAll()
+
+
+def downloadFromFile():
     imageFile=open('picture.txt')
     url="https://hub.docker.com/public/images/official/"
     dirname="pictures"
@@ -13,6 +19,23 @@ def main():
         imageName = imageName.strip()
         imageUrl=url+imageName+".png"
         os.system("wget "+imageUrl)
+
+def downloadAll():
+    config = ConfigObj("config.conf")
+    gitClient = GitClient()
+    git_dir=config["git"]["git_dir"]
+    dockerRepoVersionMaps = gitClient.getDockerRepoVersion(git_dir+"/library")
+
+    url="https://hub.docker.com/public/images/official/"
+    dirname="pictures"
+    if os.path.exists(dirname)==False:
+        os.mkdir(dirname)
+    for key in dockerRepoVersionMaps.keys():
+        print key
+        imageUrl=url+key+".png"
+        os.system("wget "+imageUrl)
+
+
 
 if __name__ == '__main__':
     main()
